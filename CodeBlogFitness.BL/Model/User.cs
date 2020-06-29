ff2730fce0b7dbf4e3a4a74b71fbc50fae6acf1c
belyh-al2@yandex.ru <belyh-al2@yandex.ru>
2020-06-29 17:57:14 +0300

    Создана модель Пользователя

---
+++ CodeBlogFitness.BL/Model/User.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CodeBlogFitness.BL.Model
+{   
+    public class User
+    {
+        #region Свойства
+        /// <summary>
+        /// Имя.
+        /// </summary>
+        public string name { get; }
+        
+        /// <summary>
+        /// Пол.
+        /// </summary>
+        public string Gender { get; }
+        
+        /// <summary>
+        /// Дата рождения.
+        /// </summary>
+        public DateTime BirthDate { get; }
+        
+        /// <summary>
+        /// Вес.
+        /// </summary>
+        public double Weight { get; set; }
+
+        /// <summary>
+        /// Рост.
+        /// </summary>
+        public double Height { get; set; }
+        #endregion
+
+        /// <summary>
+        /// Создать нового пользователя
+        /// </summary>
+        /// <param name="name"> Имя. </param>
+        /// <param name="gender"> Пол. </param>
+        /// <param name="birthDate"> Дата рождения. </param>
+        /// <param name="weight"> Вес. </param>
+        /// <param name="height"> Рост.</param>
+        public User(string name, 
+                    Gender gender, 
+                    DateTime birthDate, 
+                    double weight, 
+                    double height)
+        {
+            #region Проверка условий
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пользователя не может быть пустым или null", nameof(name));
+            }
+
+            if (Gender == null) 
+            {
+                throw new ArgumentNullException("Пол не может быть null.", nameof(gender));
+            }
+
+            if (birthDate< DateTime.Parse("01.01.1900") || birthDate>=DateTime.Now)
+            {
+                throw new ArgumentException("Невозможная дата рождения.", nameof(birthDate));
+            }
+
+            if (weight <=0)
+            {
+                throw new ArgumentException("Вес не может быть меньше нуля.", nameof(weight));
+            }
+
+            if (height <= 0)
+            {
+                throw new ArgumentException("Рост не может быть меньше нуля.", nameof(height));
+            }
+            #endregion
+
+            Name = name;
+            Gender = gender;
+            BirthDate = birthDate;
+            Weight = weight;
+            Height = height; 
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
